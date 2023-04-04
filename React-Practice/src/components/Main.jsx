296bcef2c9acceea0c1be2972f0579f0503c8a7d
Rosanna Whittle <whittlerosanna@gmail.com>
2023-04-04 14:33:31 +0100

    set up React app

---
+++ React-Practice/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Main () {
+  return (
+    <div>
+      <h1>Main goes here</h1>
+   </div>
+
+  )
+}
---
+++ React-Practice/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar () {
+  return (
+    <div>
+      <h1>Navbar goes here</h1>
+    </div>
+
+  )
+}
